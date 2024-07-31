6a98b85cb19481a71d37ee41a382ff325656b046
easily-mistaken <62795484+easily-mistaken@users.noreply.github.com>
2024-07-31 05:07:03 +0000

    Datatypes and ECMA standards

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) ?? we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Prajjwal")
+
+let name = "Prajjwal"
+let age = 18
+let isLoggedIn = false
+
+// number => 2^53
+// bigint
+// string => ""
+//boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
